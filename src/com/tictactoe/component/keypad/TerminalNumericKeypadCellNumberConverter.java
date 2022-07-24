426ec042e83f01e79b0ddbdf02be53bb564a09a7
Kostia Chaplyha <kostia71@gmail.com>
2022-07-24 21:55:42 +0300

    Update TerminalNumericKeypadCellNumberConverter

--- src/com/tictactoe/component/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/com/tictactoe/component/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -33,18 +33,12 @@ public class TerminalNumericKeypadCellNumberConverter implements CellNumberConve
 
     @Override
     public Cell toCell(final char number) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mapping[i][j] == number) {
-                    return new Cell(i, j);
-                }
-            }
-        }
-        return null;
+        final int val = number - '0' - 1;
+        return new Cell(val / 3, val % 3);
     }
 
     @Override
     public char toNumber(final Cell cell) {
-        return mapping[cell.getRow()][cell.getCol()];
+        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
     }
 }
